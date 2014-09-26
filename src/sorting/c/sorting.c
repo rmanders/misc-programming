282ff6304c30b27d0bf53b4029ed7cfc0fd35fbe
Ryan Anderson <ryan.anderson.01@gmail.com>
2014-09-25 22:25:38 -0400

    added merge

--- src/sorting/c/sorting.c
+++ src/sorting/c/sorting.c
@@ -9,19 +9,21 @@
 Generate list
 ===============================================================================
 */
-void random_list(int* list, int len) {
-    int i;    
+int* random_list(int len, int max) {
+    int i;
+    int* list;
     list = NULL;
     list = (int*) malloc (sizeof(int) * len);
     if (NULL == list) {
         printf("Out of memory. Can't generate a random list of ints\n");
-        return;
+        return NULL;
     }
 
     srand(time(NULL));
     for (i=0; i<len; i++) {
-        list[i] = rand() % INT_MAX;
+        list[i] = rand() % max;
     }
+    return list;
 }
 
 /*
@@ -36,6 +38,40 @@ void print_list(int* list, int len) {
     printf("%d,", list[i]);
   }
   printf("%d]\n", list[len-1]);
+}
+
+/*
+================================================================================
+Max
+================================================================================
+ */
+int max(int* list, int len) {
+  int max, i;
+  max = INT_MIN;
+  for (i=0; i<len; i++) {
+    if (list[i] > max)
+      max = list[i];
+  }
+  return max;
+}
+
+/*
+================================================================================
+Merge
+================================================================================
+ */
+void merge (int* list_a, int len_a, int* list_b, int len_b, int* list_c) {
+  // Merges two lists that are assumed to be sorted, resulting in a sorted list
+  int len_c, i, j, k;
+  len_c = len_a + len_b;
+  
+  i = j = k = 0;
+  while (k < len_c && i < len_a && j < len_b) {
+    if (list_a[i] < list_b[j])
+      list_c[k++] = list_b[j++];
+    else
+      list_c[k++] = list_a[i++];
+  }
 }
 
 /*
@@ -56,16 +92,29 @@ void bubble_sort(int* list, int len) {
   }
 }
 
+/*
+================================================================================
+Count Sort
+================================================================================
+*/
+void count_sort(int* list, int len);
+
+/*
+================================================================================
+Main 
+================================================================================
+ */
 int main(void) {
   int* list;
-  int len = 10;
+  int len, i, max;
   list = NULL;
+  len = 10;
+  max = 100;
   
-  printf("generating list..");
-  random_list(list, len);
-  print_list(list, len);
+  list = random_list(len, max);
 
-  printf("Sorting list...");
+  print_list(list, len);
+  printf("Sorting list...\n");
   bubble_sort(list, len);
   print_list(list, len);
 
