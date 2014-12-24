3556ad026dd4525c2f622addf011bed9a0611aeb
Ryan Anderson <ryan.anderson.01@gmail.com>
2014-12-23 21:19:22 -0500

    Tweaks to fix url mapping

--- src/java/RestApiExample/src/main/java/org/schlocknet/restapiexample/rest/Version.java
+++ src/java/RestApiExample/src/main/java/org/schlocknet/restapiexample/rest/Version.java
@@ -1,12 +1,12 @@
-package org.schocknet.restapiexample.rest;
+package org.schlocknet.restapiexample.rest;
+
 
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -14,12 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 
  */
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class Version {
   
-    @RequestMapping(method = RequestMethod.GET, value = "/version", produces = "plain/text")
-    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/version") //, produces = "plain/text")
     public String getVersion(HttpServletRequest request, HttpServletResponse response) 
 	throws IOException {
 	return "1.0.0";
